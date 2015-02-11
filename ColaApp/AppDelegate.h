177a0997a6b982098cfb1f3fcc6995f99e04fdb2
Chris Rivers <chris.rivers37@gmail.com>
2015-02-11 20:27:57 +0000

    Initial Commit

---
+++ ColaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColaApp
+//
+//  Created by Chris on 11/02/2015.
+//  Copyright (c) 2015 Chris Rivers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColaApp
+//
+//  Created by Chris on 11/02/2015.
+//  Copyright (c) 2015 Chris Rivers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
