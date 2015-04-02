9e172be538a9aeeecd911e7cac95813a7c1643dd
Chris Rivers <chris.rivers37@gmail.com>
2015-04-02 09:48:31 +0100

    Merged UI work and switches

--- ColaApp/defines.h
+++ ColaApp/defines.h
@@ -19,3 +19,7 @@ extern NSArray *moduleCatalog;
 #define ASSETS_PATH_COMPONENTS  [ASSETS_PATH stringByAppendingString:@"components/"]
 #define ASSETS_PATH_CONNECTORS  [ASSETS_PATH stringByAppendingString:@"connectors/"]
 #define ASSETS_PATH_CONTROLS    [ASSETS_PATH stringByAppendingString:@"encoders/"]
+
+#define TOOLBAR_BUILD_ICON_SELECTED         @"Icons/742-wrench-toolbar-selected"
+#define TOOLBAR_BUILD_ICON                  @"Icons/742-wrench-toolbar"
+#define TOOLBAR_KEYBOARD_ICON_SELECTED      @"Icons/967-piano-toolbar-selected"
