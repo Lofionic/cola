81ee46ca2e434b475dcc16693bf0a3600f1f2d1e
Chris Rivers <chris.rivers37@gmail.com>
2015-11-14 20:15:48 +0000

    Added LFO

---
+++ ColaLib/ColaLib/CCOL/CCOLComponentLFO.cpp
@@ -0,0 +1,9 @@
+//
+//  CCOLComponentLFO.cpp
+//  ColaLib
+//
+//  Created by Chris on 14/11/2015.
+//  Copyright © 2015 Chris Rivers. All rights reserved.
+//
+
+#include "CCOLComponentLFO.hpp"
---
+++ ColaLib/ColaLib/CCOL/CCOLComponentLFO.hpp
@@ -0,0 +1,14 @@
+//
+//  CCOLComponentLFO.hpp
+//  ColaLib
+//
+//  Created by Chris on 14/11/2015.
+//  Copyright © 2015 Chris Rivers. All rights reserved.
+//
+
+#ifndef CCOLComponentLFO_hpp
+#define CCOLComponentLFO_hpp
+
+#include <stdio.h>
+
+#endif /* CCOLComponentLFO_hpp */
